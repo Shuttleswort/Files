1364746ddcf43d54e6a154da759e313c8a41b825
V.Abdrakhmanov <vladimira@2kgroup.com>
2018-07-17 16:55:12 +0300

    Initial Commit

---
+++ Files/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Files
+//
+//  Created by Vladimir Abdrakhmanov on 7/17/18.
+//  Copyright © 2018 Vladimir Abdrakhmanov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
